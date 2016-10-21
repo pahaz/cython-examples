a86315eaec2c44c4589edc8f9ffb6fa2c159fa5c
pahaz <pahaz.blinov@gmail.com>
2016-10-21 21:48:53 +0500

    feat: init 01-compile-pyx

---
+++ 01-compile-pyx/example1.pyx
@@ -0,0 +1,13 @@
+cdef int increment(int i):
+    return i + 1
+
+print('increment(5) ->', increment(5))
+
+cdef int a[3], i
+a[:] = [0, 0, 0]
+cdef bint xxx = False
+
+for i in range(10):
+    a[i % 3] += i
+
+print(a)
